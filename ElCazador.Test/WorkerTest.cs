1f1987b2fad8ff8a3323c06e142c308e38b76c78
Søren Fritzbøger <thebrunodev@gmail.com>
2019-01-17 17:43:15 +0100

    Removed an unused test

--- ElCazador.Test/WorkerTest.cs
+++ ElCazador.Test/WorkerTest.cs
@@ -1,50 +1,50 @@
-using System;
-using Xunit;
-using ElCazador.Worker;
-using ElCazador.Worker.Models;
-using ElCazador.Worker.Modules.Tools;
-using Moq;
-using ElCazador.Worker.Interfaces;
+// using System;
+// using Xunit;
+// using ElCazador.Worker;
+// using ElCazador.Worker.Models;
+// using ElCazador.Worker.Modules.Tools;
+// using Moq;
+// using ElCazador.Worker.Interfaces;
 
-namespace ElCazador.Test
-{
-    public class WorkerTest
-    {
-        private Mock<IWorkerController> WorkerControllerMock;
+// namespace ElCazador.Test
+// {
+//     public class WorkerTest
+//     {
+//         private Mock<IWorkerController> WorkerControllerMock;
 
-        public WorkerTest()
-        {
-            WorkerControllerMock = new Mock<IWorkerController>();
+//         public WorkerTest()
+//         {
+//             WorkerControllerMock = new Mock<IWorkerController>();
 
-            WorkerControllerMock.Setup((x) => x.WorkerSettings.ImpacketExamplesPath).Returns("/Users/elpatron/Documents/Tools/impacket/examples/");
+//             WorkerControllerMock.Setup((x) => x.WorkerSettings.ImpacketExamplesPath).Returns("/Users/elpatron/Documents/Tools/impacket/examples/");
 
             
-        }
+//         }
 
-        [Fact]
-        public void TestLsassDump()
-        {
-            var target = new Target
-            {
-                Hostname = "PWNABLE10",
-                IP = "10.64.13.134",
-                Dumped = false,
-                Timestamp = DateTime.UtcNow
-            };
+//         [Fact]
+//         public void TestLsassDump()
+//         {
+//             var target = new Target
+//             {
+//                 Hostname = "PWNABLE10",
+//                 IP = "10.64.13.134",
+//                 Dumped = false,
+//                 Timestamp = DateTime.UtcNow
+//             };
 
-            var user = new User
-            {
-                Username = "Bente",
-                Domain = "PWNTHIS",
-                Hash = "Headshot1",
-                PasswordType = "ClearText"
-            };
+//             var user = new User
+//             {
+//                 Username = "Bente",
+//                 Domain = "PWNTHIS",
+//                 Hash = "Headshot1",
+//                 PasswordType = "ClearText"
+//             };
 
-            var lsassDumpTool = new LsassDumpTool(WorkerControllerMock.Object, "LsassDump");
+//             var lsassDumpTool = new LsassDumpTool(WorkerControllerMock.Object, "LsassDump");
 
-            Assert.Equal("/Users/elpatron/Documents/Tools/impacket/examples/", WorkerControllerMock.Object.WorkerSettings.ImpacketExamplesPath);
+//             Assert.Equal("/Users/elpatron/Documents/Tools/impacket/examples/", WorkerControllerMock.Object.WorkerSettings.ImpacketExamplesPath);
 
-            lsassDumpTool.Run(target, user);
-        }
-    }
-}
+//             lsassDumpTool.Run(target, user);
+//         }
+//     }
+// }
